a0c392b166c354c7204027c176583899d1d4394d
Ashveer Ramparsad <ashveer12@gmail.com>
2022-06-03 08:51:09 +0200

    220603_0850

---
+++ app/src/main/java/za/smartee/threesixty/activity/AssetAllocationsActivity.java
@@ -0,0 +1,15 @@
+package za.smartee.threesixty.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import za.smartee.threesixty.R;
+
+public class AssetAllocationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asset_allocations);
+    }
+}
